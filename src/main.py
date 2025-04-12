5296e5892a5e04af79d61a88702f9c9b7ca7fe97
Juan Cruz <jcuvabritos@gmail.com>
2025-04-12 13:48:49 -0300

    restructuring the code organization

--- src/main.py
+++ src/main.py
@@ -1,111 +1,110 @@
-def main(): 
-    
-    class Number:
+class Number:   
+    def __init__(self,num,base):
+        self.num = num
+        self.base = base
         
-        def __init__(self,num,base):
-            self.num = num
-            self.base = base
+    def base_change(self,new_base): 
         
-        def base_change(self,new_base): 
-            
-            #decimal to another base
-            if self.base == 10:
-                quotient = int(self.num)
-                remainder_list = []
-            
-                if quotient == 0:
-                    converted_num = "0"
-                else:
-                    while quotient != 0:
-                        remainder = quotient % new_base
-                        if new_base == 16: 
-                            if remainder == 10: remainder = 'A'
-                            elif remainder == 11: remainder = 'B'
-                            elif remainder == 12: remainder = 'C'
-                            elif remainder == 13: remainder = 'D'
-                            elif remainder == 14: remainder = 'E'
-                            elif remainder == 15: remainder = 'F'
-                        
-                        remainder_list.append(remainder)
-                        
-                        quotient = quotient // new_base
+        #decimal to another base
+        if self.base == 10:
+            quotient = int(self.num)
+            remainder_list = []
+        
+            if quotient == 0:
+                converted_num = "0"
+            else:
+                while quotient != 0:
+                    remainder = quotient % new_base
+                    if new_base == 16: 
+                        if remainder == 10: remainder = 'A'
+                        elif remainder == 11: remainder = 'B'
+                        elif remainder == 12: remainder = 'C'
+                        elif remainder == 13: remainder = 'D'
+                        elif remainder == 14: remainder = 'E'
+                        elif remainder == 15: remainder = 'F'
                     
-                    remainder_list.reverse()
+                    remainder_list.append(remainder)
                     
-                    converted_num = ("".join(map(str,remainder_list)))
-                    
-                    if self.num == int(self.num):
-                        return converted_num
-                    else: 
-                        int_num = int(self.num)
-                        dec_num = self.num - int_num
-                        dec_num_list = []
-                        
-                        lenght = len(str(dec_num)) - 2
-                        
-                        for i in range(0,lenght):
-                            int_dec_num = int(dec_num)
-                            dec_num = dec_num - int_dec_num
-                            
-                            dec_num = dec_num * new_base
-                            
-                            dec_num_list.append(int(dec_num))
-                        
-                        dec_converted = ("".join(map(str,dec_num_list)))
-                        
-                        if converted_num == str: 
-                            converted_num = converted_num + "."+ dec_converted
-                        else: converted_num = "0."+ dec_converted
-                        return converted_num
-
-            #other systems to decimal
-            if new_base == 10: 
-                converted_num = float()
-                number = str(self.num)
+                    quotient = quotient // new_base
                 
+                remainder_list.reverse()
                 
-                if '.' in number:
-                    point_index = number.index('.')
-                    int_part = number[0:point_index]
-                    dec_part = number[point_index+1:len(number)]
-                    number = int_part + dec_part
-                    exponent = len(int_part)-1
+                converted_num = ("".join(map(str,remainder_list)))
+                
+                if self.num == int(self.num):
+                    return converted_num
                 else: 
-                    exponent = len(number)-1
-                
-                
-                for i in number:
-                    if i == 'A': i = 10
-                    elif i == 'B': i == 11
-                    elif i == 'C': i == 12
-                    elif i == 'D': i == 13
-                    elif i == 'E': i == 14
-                    elif i == 'F': i == 15
-                    converted_num += int(i) * (self.base ** exponent)
+                    int_num = int(self.num)
+                    dec_num = self.num - int_num
+                    dec_num_list = []
                     
-                    exponent -= 1
+                    lenght = len(str(dec_num)) - 2
+                    
+                    for i in range(0,lenght):
+                        int_dec_num = int(dec_num)
+                        dec_num = dec_num - int_dec_num
+                        
+                        dec_num = dec_num * new_base
+                        
+                        dec_num_list.append(int(dec_num))
+                    
+                    dec_converted = ("".join(map(str,dec_num_list)))
+                    
+                    if converted_num == str: 
+                        converted_num = converted_num + "."+ dec_converted
+                    else: converted_num = "0."+ dec_converted
+                    return converted_num
+        #other systems to decimal
+        if new_base == 10: 
+            converted_num = float()
+            number = str(self.num)
+            
+            
+            if '.' in number:
+                point_index = number.index('.')
+                int_part = number[0:point_index]
+                dec_part = number[point_index+1:len(number)]
+                number = int_part + dec_part
+                exponent = len(int_part)-1
+            else: 
+                exponent = len(number)-1
+            
+            
+            for i in number:
+                if i == 'A': i = 10
+                elif i == 'B': i == 11
+                elif i == 'C': i == 12
+                elif i == 'D': i == 13
+                elif i == 'E': i == 14
+                elif i == 'F': i == 15
+                converted_num += int(i) * (self.base ** exponent)
                 
-                return converted_num
-    
-    def addition(base,*number):
-        result = 0
-        
-        for i in number:
-            result += float(i.base_change(10))
-        
-        
-        new_num = Number(result,10)
-        return new_num.base_change(base)
+                exponent -= 1
+            
+            return converted_num
 
-    def subtraction(base,*number):
-        result = float(number[0].base_change(10))
-        
-        for i in number[1:]:
-            result -= float(i.base_change(10))
-        
-        
-        new_num = Number(result,10)
-        return new_num.base_change(base)
+def addition(base,*number):
+    result = 0
+    
+    for i in number:
+        result += float(i.base_change(10))
+    
+    
+    new_num = Number(result,10)
+    return new_num.base_change(base)
+
+def subtraction(base,*number):
+    result = float(number[0].base_change(10))
+    
+    for i in number[1:]:
+        result -= float(i.base_change(10))
+    
+    
+    new_num = Number(result,10)
+    return new_num.base_change(base)
+
+def main(): 
+    pass
 
 if __name__ == '__main__':
     main()
